d2b0c47eb5818e1e41bf2234c613988e976783d5
Felix G <76596162+API-Troubles@users.noreply.github.com>
2024-07-16 13:42:11 +0000

    Created utils.py, moved functions from main.py there

---
+++ utils.py
@@ -0,0 +1,13 @@
+# A seperated space for utility functions, keeps main.py more organized!
+
+def convert_symbol(text: str) -> str:
+    if text not in list(currency_options.keys()):
+        return list(currency_options.keys())[list(currency_options.values()).index(text)]
+    return text
+
+
+def find_match(orignal: str, options: dict) -> str:
+    match, confidence = process.extractOne(orignal, options) # type: ignore
+    if confidence < 90:
+        raise ValueError(f"No currency found for '{orignal}'. Did you mean '{match}'?")
+    return match
